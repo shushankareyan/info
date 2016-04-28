f3a15926b8cd1b6824f697425db3c0dcc7d43c44
Hayk_Grigoryan <hayk.g.grigoryan@monitis.com>
2016-04-28 14:36:46 +0400

    asa

--- application/views/Post/CreatePost.php
+++ application/views/Post/CreatePost.php
@@ -6,13 +6,23 @@
  * and open the template in the editor.
  */
 ?>
+<?php
+if ( empty($categories)){
+    ?>
+        <div>
+            <h3>
+                You don't have categories. Please add category at first
+            </h3>
+        </div>
+
+    <?php
+
+} else {
+?>
+
 <form id="Post" name="Post" action="<?php echo site_url("post/create"); ?>" method="post">
 		<h2> Post </h2>
-                 <?php
-                    if ( empty($categories)){
-                        echo "You don't have categories. Please add category at first";
-                    } else {
-                ?>
+
 		<p> 
 			<label for="postName"  >Name </label>
 			<input class="input" name ="postName" type ="text">
@@ -29,9 +39,8 @@
                         ?>
                     </select>
 		</p>
-                
 		<input type="submit" value="Create" name="">
-                   <?php
-                            }
-                        ?>
 </form>
+    <?php
+}
+?>
